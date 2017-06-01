48a8e1672a3556dd71e011a45d48bff9218d0231
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-06-01 15:54:21 +0200

    Addded the processing of requests.

--- src/main/java/nl/esciencecenter/praline/network/WebServer.java
+++ src/main/java/nl/esciencecenter/praline/network/WebServer.java
@@ -21,11 +21,21 @@ public class WebServer {
 
     public void run() {
         post("/send/:sequence", (request, response) -> {
-            Sequence sequence = new Sequence(request.params(":sequence"), 0);
-            sequences.add(sequence);
-            return "OK";
+            int statusCode = processSendSequence(request.params(":sequence"), request.body());
+            response.status(statusCode);
+            return "Sequence " + request.params(":sequence") + " processed.";
         });
-        get("/receive/:sequence1/:sequence2", (request, response) -> { return "Score: " + request.params(":sequence1") + " " + request.params(":sequence2");});
+        get("/receive/:sequence1/:sequence2", (request, response) -> {
+            ScoreMatrix score = scores.get(request.params(":sequence1") + request.params(":sequence2"));
+            if ( score != null ) {
+                response.status(200);
+                return score;
+            } else {
+                response.status(404);
+                return "";
+            }
+        });
+        // Compress responses
         after(((request, response) -> { response.header("Content-Encoding", "gzip"); }));
     }
 
@@ -39,5 +49,21 @@ public class WebServer {
 
     public void setScores(HashMap<String, ScoreMatrix> scores) {
         this.scores = scores;
+    }
+
+    private int processSendSequence(String id, String body) {
+        Sequence sequence;
+        ArrayList<Integer> numericSequence = new ArrayList<>();
+
+        for ( String item : body.split(" ") ) {
+            numericSequence.add(Integer.parseInt(item));
+        }
+        sequence = new Sequence(id, numericSequence.size());
+        for ( int item = 0; item < numericSequence.size(); item++ ) {
+            sequence.setElement(item, numericSequence.get(item));
+        }
+        sequences.add(sequence);
+
+        return 201;
     }
 }

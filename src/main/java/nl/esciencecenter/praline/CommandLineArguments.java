f0bbda37c9bb138d36fb1ddc7ca9ac1a8fd17521
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-06-14 14:48:07 +0200

    Added another command line parameter. Its purpose is to limit the number of accepted sequences.

--- src/main/java/nl/esciencecenter/praline/CommandLineArguments.java
+++ src/main/java/nl/esciencecenter/praline/CommandLineArguments.java
@@ -5,8 +5,14 @@ import com.beust.jcommander.Parameter;
 public class CommandLineArguments {
     @Parameter(names = {"-server_threads"}, description = "Number of threads for the web server")
     private Integer serverThreads = 42;
+    @Parameter(names = {"-expected_sequences"}, description = "Number of sequences to expect")
+    private Integer expectedSequences = -1;
 
     public int getNrServerThreads() {
         return serverThreads;
+    }
+
+    public int getNrExpectedSequences() {
+        return expectedSequences;
     }
 }

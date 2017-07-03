477385a81c43e999342ac84c2dc3a608ef6293ce
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-07-03 14:09:30 +0200

    First implementation of the Alphabet.

---
+++ src/main/java/nl/esciencecenter/praline/containers/Alphabet.java
@@ -0,0 +1,28 @@
+package nl.esciencecenter.praline.containers;
+
+public class Alphabet {
+    private String name;
+    private int length;
+
+    public Alphabet(String name) {
+        this.name = name;
+        length = 0;
+    }
+
+    public Alphabet(String name, int length) {
+        this.name = name;
+        this.length = length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        length = length;
+    }
+}

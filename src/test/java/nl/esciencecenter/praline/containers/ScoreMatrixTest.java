5c744d25f005897b3b884b8124f2bc8eca79c902
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-07-05 10:52:30 +0200

    First test for ScoreMatrix, still does not cover all methods.

---
+++ src/test/java/nl/esciencecenter/praline/containers/ScoreMatrixTest.java
@@ -0,0 +1,35 @@
+package nl.esciencecenter.praline.containers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScoreMatrixTest {
+    private String matrixName = "MatrixName";
+    private String alphabetName = "AlphabetName";
+    private int alphabetLength = 32;
+
+    @Test
+    public void getName() {
+        ScoreMatrix matrix = new ScoreMatrix(matrixName);
+
+        assertEquals(matrixName, matrix.getName());
+    }
+
+    @Test
+    public void getAlphabet() {
+        Alphabet alphabet = new Alphabet(alphabetName, alphabetLength);
+        ScoreMatrix matrix = new ScoreMatrix(matrixName, alphabet);
+
+        assertEquals(alphabetName, matrix.getAlphabet().getName());
+    }
+
+    @Test
+    public void setAlphabet() {
+        Alphabet alphabet = new Alphabet(alphabetName, alphabetLength);
+        ScoreMatrix matrix = new ScoreMatrix(matrixName);
+
+        matrix.setAlphabet(alphabet);
+        assertEquals(alphabetName, matrix.getAlphabet().getName());
+    }
+}

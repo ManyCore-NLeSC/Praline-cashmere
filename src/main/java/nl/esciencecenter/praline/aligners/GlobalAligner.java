c3da82e801c165d11dfd1cee1bdd6d8bb109c6f1
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-07-17 13:23:51 +0200

    Fixing bugs in the aligner, and adding a unit test for it.

--- src/main/java/nl/esciencecenter/praline/aligners/GlobalAligner.java
+++ src/main/java/nl/esciencecenter/praline/aligners/GlobalAligner.java
@@ -6,7 +6,6 @@ import nl.esciencecenter.praline.data.Move;
 // Needleman–Wunsch algorithm
 public class GlobalAligner {
     private float gapScore;
-    private AlignmentMatrix matrix;
 
     public GlobalAligner() {
         gapScore = -2.0f;
@@ -14,15 +13,6 @@ public class GlobalAligner {
 
     public GlobalAligner(float gapScore) {
         this.gapScore = gapScore;
-    }
-
-    public GlobalAligner(AlignmentMatrix matrix) {
-        this.matrix = matrix;
-    }
-
-    public GlobalAligner(float gapScore, AlignmentMatrix matrix) {
-        this.gapScore = gapScore;
-        this.matrix = matrix;
     }
 
     public float getGapScore() {
@@ -33,46 +23,46 @@ public class GlobalAligner {
         this.gapScore = gapScore;
     }
 
-    public void setMatrix(AlignmentMatrix matrix) {
-        this.matrix = matrix;
-    }
-
-    public void initializeMatrix() {
-        matrix.setScore(0, 0.0f);
-        for ( int item = 1; item < matrix.getSequence(0).getLength() + 1; item++ ) {
-            matrix.setScore(item, matrix.getScore(item - 1) + gapScore);
-        }
-        for ( int item = 1; item < matrix.getSequence(1).getLength() + 1; item++) {
-            matrix.setScore(item, matrix.getScore(item * matrix.getSequence(0).getLength()) + gapScore);
-        }
-    }
-
-    public void computeAlignment() {
+    public void computeAlignment(AlignmentMatrix matrix) {
+        initialize(matrix);
         for ( int row = 1; row < matrix.getSequence(1).getLength() + 1; row++ ) {
             for ( int column = 1; column < matrix.getSequence(0).getLength() + 1; column++ ) {
                 float tempScore = 0.0f;
                 float bestScore = 0.0f;
                 Move bestMove = Move.NIL;
 
-                if ( (matrix.getScore(((row - 1) * matrix.getSequence(0).getLength()) + column) + gapScore) > (matrix.getScore((row * matrix.getSequence(0).getLength()) + (column - 1)) + gapScore) ) {
-                    bestScore = matrix.getScore(((row - 1) * matrix.getSequence(0).getLength()) + column) + gapScore;
+                if ( (matrix.getScore(((row - 1) * (matrix.getSequence(0).getLength()) + 1) + column) + gapScore) > (matrix.getScore((row * (matrix.getSequence(0).getLength() + 1)) + (column - 1)) + gapScore) ) {
+                    bestScore = matrix.getScore(((row - 1) * (matrix.getSequence(0).getLength() + 1)) + column) + gapScore;
                     bestMove = Move.TOP;
                 } else {
-                    bestScore = matrix.getScore((row * matrix.getSequence(0).getLength()) + (column - 1)) + gapScore;
+                    bestScore = matrix.getScore((row * (matrix.getSequence(0).getLength() + 1)) + (column - 1)) + gapScore;
                     bestMove = Move.LEFT;
                 }
                 if ( matrix.getSequence(0).getElement(column) == matrix.getSequence(1).getElement(row) ) {
-                    tempScore = matrix.getScore(((row - 1) * matrix.getSequence(0).getLength()) + (column - 1)) + 1.0f;
+                    tempScore = matrix.getScore(((row - 1) * (matrix.getSequence(0).getLength() + 1)) + (column - 1)) + 1.0f;
                 } else {
-                    tempScore = matrix.getScore(((row - 1) * matrix.getSequence(0).getLength()) + (column - 1)) - 1.0f;
+                    tempScore = matrix.getScore(((row - 1) * (matrix.getSequence(0).getLength() + 1)) + (column - 1)) - 1.0f;
                 }
                 if ( tempScore  > bestScore ) {
                     bestScore = tempScore;
                     bestMove = Move.TOP_LEFT;
                 }
-                matrix.setScore((row * matrix.getSequence(0).getLength()) + column, bestScore);
-                matrix.setMove((row * matrix.getSequence(0).getLength()) + column, bestMove);
+                matrix.setScore((row * (matrix.getSequence(0).getLength() + 1)) + column, bestScore);
+                matrix.setMove((row * (matrix.getSequence(0).getLength() + 1)) + column, bestMove);
             }
+        }
+    }
+
+    private void initialize(AlignmentMatrix matrix) {
+        matrix.setScore(0, 0.0f);
+        matrix.setMove(0, Move.NIL);
+        for ( int item = 1; item < matrix.getSequence(0).getLength() + 1; item++ ) {
+            matrix.setScore(item, matrix.getScore(item - 1) + gapScore);
+            matrix.setMove(item, Move.LEFT);
+        }
+        for ( int item = 1; item < matrix.getSequence(1).getLength() + 1; item++) {
+            matrix.setScore(item * (matrix.getSequence(0).getLength() + 1), matrix.getScore((item - 1) * (matrix.getSequence(0).getLength() + 1)) + gapScore);
+            matrix.setMove(item * (matrix.getSequence(0).getLength() + 1), Move.TOP);
         }
     }
 }
---
+++ src/test/java/nl/esciencecenter/praline/aligners/GlobalAlignerTest.java
@@ -0,0 +1,46 @@
+package nl.esciencecenter.praline.aligners;
+
+import nl.esciencecenter.praline.data.AlignmentMatrix;
+import nl.esciencecenter.praline.data.Move;
+import nl.esciencecenter.praline.data.Sequence;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GlobalAlignerTest {
+    private final float epsilon = 0.001f;
+
+    @Test
+    public void getGapScore() {
+        GlobalAligner aligner = new GlobalAligner();
+        assertEquals(-2.0f, aligner.getGapScore(), epsilon);
+        aligner.setGapScore(-42.3f);
+        assertEquals(-42.3f, aligner.getGapScore(), epsilon);
+    }
+
+    @Test
+    public void alignment() {
+        Sequence sequenceOne = new Sequence("SequenceOne");
+        Sequence sequenceTwo = new Sequence("SequenceTwo");
+        AlignmentMatrix matrix = new AlignmentMatrix("Test");
+        GlobalAligner aligner = new GlobalAligner();
+
+        sequenceOne.setElements(new int [] {3, 2, 2, 2, 1, 0, 3, 2, 3});
+        sequenceTwo.setElements(new int [] {3, 2, 3, 2, 0, 0, 1, 3});
+        matrix.addSequence(sequenceOne);
+        matrix.addSequence(sequenceTwo);
+        matrix.allocateMatrix();
+        aligner.computeAlignment(matrix);
+
+        assertEquals(0.0f, matrix.getScore(0), epsilon);
+        assertEquals(Move.NIL, matrix.getMove(0));
+        assertEquals(-18.0f, matrix.getScore(sequenceOne.getLength()), epsilon);
+        assertEquals(Move.LEFT, matrix.getMove(sequenceOne.getLength()));
+        assertEquals(-16.0f, matrix.getScore(sequenceTwo.getLength() * (sequenceOne.getLength() + 1)), epsilon);
+        assertEquals(Move.TOP, matrix.getMove(sequenceTwo.getLength() * (sequenceOne.getLength() + 1)));
+        assertEquals(0.0f, matrix.getScore((sequenceTwo.getLength() * (sequenceOne.getLength() + 1)) + sequenceOne.getLength()), epsilon);
+        assertEquals(Move.TOP_LEFT, matrix.getMove((sequenceTwo.getLength() * (sequenceOne.getLength() + 1)) + sequenceOne.getLength()));
+        assertEquals(-8.0f, matrix.getScore((2 * (sequenceOne.getLength() + 1)) + 7), epsilon);
+        assertEquals(Move.LEFT, matrix.getMove((2 * (sequenceOne.getLength() + 1)) + 7));
+    }
+}

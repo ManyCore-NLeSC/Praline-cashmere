086562b8ee070270b0a97af2086a5834919f2a71
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-08-28 14:24:38 +0200

    First implementation of getAlignment().

--- src/main/java/nl/esciencecenter/praline/data/LocalAlignmentMatrix.java
+++ src/main/java/nl/esciencecenter/praline/data/LocalAlignmentMatrix.java
@@ -18,7 +18,23 @@ public class LocalAlignmentMatrix extends  AlignmentMatrix{
     }
 
     public ArrayList<String> getAlignment() {
+        int row = maxScoreIndex / (getSequence(1).getLength() + 1);
+        int column = maxScoreIndex % (getSequence(0).getLength() + 1);
         ArrayList<String> alignment = new ArrayList<>();
+
+        while ( getMove((row * (getSequence(0).getLength() + 1)) + column) != Move.NIL ) {
+            Move move = getMove((row * (getSequence(0).getLength() + 1)) + column);
+            alignment.add(String.valueOf(row) + " " + String.valueOf(column));
+            if ( move == Move.TOP ) {
+                row -= 1;
+            } else if ( move == Move.LEFT ) {
+                column -= 1;
+            } else {
+                row -= 1;
+                column -= 1;
+            }
+        }
+        alignment.add(String.valueOf(row) + " " + String.valueOf(column));
 
         return alignment;
     }

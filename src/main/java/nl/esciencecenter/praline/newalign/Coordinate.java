6ed2fd337be16613eacf47bfa0c96a547609f66e
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-02-14 10:02:29 +0100

    Made members private and added a method to convert coordinates to strings.

--- src/main/java/nl/esciencecenter/praline/newalign/Coordinate.java
+++ src/main/java/nl/esciencecenter/praline/newalign/Coordinate.java
@@ -1,11 +1,24 @@
 package nl.esciencecenter.praline.newalign;
 
 public class Coordinate {
-    final public int x;
-    final public int y;
+    private final int x;
+    private final int y;
 
     public Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    @Override
+    public String toString() {
+        return this.x + ";" + this.y;
     }
 }
